c490a7ccc471a4b7d85bdd4fc65b8d3c12b2f11c
test <test>
2017-07-31 23:08:09 +0400

    sdfgg

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 'secret': 'MyDemoRestApiEcryptedKey',
-'database': 'mongodb://localhost/mean-dev1'
+'database': 'mongodb://josh_expresswriters:test123456789@ds129043.mlab.com:29043/mean-dev1'
    // mongodb://<dbuser>:<dbpassword>@ds145669.mlab.com:45669/uvi
 };
